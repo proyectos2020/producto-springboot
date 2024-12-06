2aa2f92d85939dfba9939903bd9d431fa2ff405e
wilmarca007 <wilmarca007@gmail.com>
2024-12-06 09:34:47 -0500

    Primer commit - Proyecto Spring Boot

---
+++ src/test/java/prueba/tecnica/producto/ProductoApplicationTests.java
@@ -0,0 +1,13 @@
+package prueba.tecnica.producto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
